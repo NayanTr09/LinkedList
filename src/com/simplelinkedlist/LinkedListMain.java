ebef1c9113f491bc6e45038ac497e938b1dcf0ec
NayanTr09 <nayantripathi42@gmail.com>
2022-01-06 14:49:32 +0530

    Added : Delete First Node Functionality

--- src/com/simplelinkedlist/LinkedListMain.java
+++ src/com/simplelinkedlist/LinkedListMain.java
@@ -3,6 +3,7 @@ package com.simplelinkedlist;
 public class LinkedListMain {
     public static void main(String[] args)
     {
+        Node head = null;
         LinkedList llist = new LinkedList();
         llist.push(70);
         llist.push(30);
@@ -16,6 +17,12 @@ public class LinkedListMain {
 
         llist.insertAfter(llist.head.next, 8);
         System.out.println("\nCreated Linked list is: ");
+        llist.printList();
+
+        llist.deleteNode(56); // Delete node with data 1
+
+        System.out.println(
+                "\nLinked List after Deletion of 1:");
         llist.printList();
     }
 }
